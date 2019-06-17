71d2dd78c9da0be7f0818e1fa278c35eb378cf23
Ann <ggvv0413@gmail.com>
2019-06-17 10:39:18 +0900

    css update / product , detail page publishing

--- src/components/detail.vue
+++ src/components/detail.vue
@@ -2,39 +2,51 @@
   <div>
     <Header/>
     <div class="position-relative pt-5 pb-4">
-      <div class="portfolio-image" style="z-index: -1; background-image: url(assets/img/bg/25.jpg);"></div>
+      <div class="portfolio-image" style="z-index: -1;"></div>
       <div class="align-items-end py-5 px-4 px-md-5">
         <div class="font-secondary py-md-4 py-xl-5 mx-lg-5">
           <div class="portfolio-caption text-white font-weight-semibold"><div class="text-tiny font-weight-light">IT ALWAYS SEEMS<br>IMPOSSIBLE UNTIL<br>IT'S DONE</div></div>
         </div>
       </div>
     </div>
-    <div class="portfolio-block container px-3">
-      <div class="row justify-content-between">
-        <div class="col-lg-5">
-          <div class="small mb-4">
-            <a href="#" class="text-muted font-weight-semibold">SOME COMPANY</a>
-            <span class="text-lighter">&nbsp;\&nbsp;</span>
-            <span class="text-lighter">2018</span>
+    <div class="container px-3 my-4">
+      <h1 class="mb-4">
+        TWICE
+      </h1>
+      <div class="flex">
+        <div class="flex-box-1" style="flex-basis: 45%;">
+          <div class="small" style="font-size: 15px;">
+            <p>올해 2월 스페셜 앨범 [KNOCK KNOCK]으로 팬들의 마음을 두드렸던 트와이스가 드디어 박진영의 곡으로 5월 메이퀸 등극을 예고하며 3개월만에 팬들 앞에 미니 4집 [SIGNAL]을 선보인다. '우아하게' – 'CHEER UP' – 'TT' – 'KNOCK KNOCK'까지 4연속 메가 히트와 더불어 K-POP 걸그룹 최다 유튜브 조회수, 1억뷰 돌파, 30만장에 육박하는 음반 판매고, 첫 콘서트 실시간 매진 등 '기록 제조기'로서의 명성에 모자람이 없는 성적들을 만들어 온 트와이스가 이번에는 또 어떤 성과를 만들어 낼지 네이버 뮤직 스페셜에서 단독으로 공개되는 B컷을 감상하며 기대해보자.</p>          
           </div>
-          <h1 class="mb-4">
-            Contemporary branding and photography for a clothing brand
-          </h1>
-          <div class="small">
-            <a href="#" class="text-dark">BRANDING</a>
-            &nbsp; <span class="text-lighter">+</span> &nbsp;
-            <a href="#" class="text-dark">PHOTOGRAPHY</a>
+          <div class="flex" style="margin-top: 15px;">
+            <button type="submit" class="button-submit app" style="flex-basis: 50%;">프로젝트 후원하기</button>
+            <button type="submit" class="button-submit app" style="flex-basis: 50%; margin-left: 1px;">좋아요</button>
           </div>
         </div>
-        <div class="col-lg-6 pt-5 pt-lg-0">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce interdum mauris tellus, eu finibus tortor dictum vel.</p>
-          <p>Cras id nisi et dui fermentum feugiat. Etiam luctus ligula eget augue feugiat rutrum. Nulla sed est in orci maximus mattis vitae vitae libero. Proin varius fermentum lobortis.</p>
-          <p>Praesent tristique elementum nulla eget commodo. Pellentesque mattis sollicitudin leo et cursus.</p>
-          <p class="mb-0">Nullam bibendum risus eu varius finibus. Maecenas nec ipsum blandit, gravida orci eu, molestie urna. Ut vel justo tempor, aliquet lacus at, condimentum purus.</p>
-        </div>
+          <div class="overview_cnt" style="flex-basis: 45%; margin-left: 100px;">
+            <table>
+              <tr>
+                <th scope="row">모인 금액</th>
+                <td><span class="h1 mb4">7,777,700</span>원</td>
+              </tr>
+              <tr>
+                <th scope="row">남은 기간</th>
+                <td><span class="h1 mb4">24</span>일</td>
+              </tr><tr>
+                <th scope="row">후원자</th>
+                <td><span class="h1 mb4">789</span>명</td>
+              </tr><tr>
+                <th scope="row">좋아요</th>
+                <td><span class="h1 mb4">123,456</span>개</td>
+              </tr>
+            </table>
+            <div class="layout-guide-box">
+              <i class="fa fa-lightbulb-o" />
+              <span> 목표금액인 777,777,000원이 모이면 데뷔에 가까워집니다. <br>후원자 모두에게는 매주 관련 뉴스레터를 보내드립니다.</span>
+            </div>
+          </div>
       </div>
-      <!-- Skills -->
-    <div class="container portfolio-block-padding-t px-3">
+    <!-- <div class="container portfolio-block-padding-t px-3">
       <h4 class="font-secondary text-center font-weight-semibold m-0">SKILLS</h4>
 
       <div class="row">
@@ -84,8 +96,7 @@
           </div>
         </div>
       </div>
-    </div>
-    <!-- / Skills -->
+    </div> -->
     </div>
   </div>
 </template>
@@ -100,7 +111,7 @@ export default {
 </script>
 
 <style scoped>
-  body {
-    overflow-y: scroll;
+  .portfolio-image {
+    background-image: url('../assets/img2/bg/25.jpg');
   }
 </style>
--- src/components/product.vue
+++ src/components/product.vue
@@ -1,216 +1,90 @@
 <template>
-<!-- Products -->
-  <div class="ui-bordered">
-    <Header/>
-    <div class="blank"></div>
-    <div class="shop-list-row row row-bordered no-gutters">
-      <a href="#" class="d-block col-md-3 col-xl-2 text-center p-4 pr-md-0">
-        <img src="assets/img/uikit/ps4.jpg" alt class="img-fluid" style="max-height: 190px">
-      </a>
-      <div class="shop-list-content col-md-6 col-xl-7 py-md-4 px-4 pb-2">
-        <div class="d-flex justify-content-start mb-1">
-          <h5 class="flex-shrink-1 m-0">
-            <a href="#" class="text-dark">PlayStation 4 1TB (Jet Black)</a>
-          </h5>
-          <a href="#" class="shop-tooltip d-inline-block text-lightest small ml-4" title="Share"><i class="ion ion-md-share"></i></a>
-          <a href="#" class="shop-tooltip d-inline-block text-lightest small ml-3" title="Add to wishlist"><i class="ion ion-ios-heart"></i></a>
-        </div>
-        <div class="mb-2">
-          <div class="ui-stars">
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star half-filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
+  <div class="container px-3 my-4">
+    <div class="ui-bordered">
+      <Header/>
+      <div class="blank"></div>
+      <div class="shop-list-row row row-bordered no-gutters">
+        <a href="#" class="d-block col-md-3 col-xl-2 text-center p-4 pr-md-0">
+          <img src="@/assets/img2/uikit/ps4.jpg" alt class="img-fluid" style="max-height: 190px">
+        </a>
+        <div class="shop-list-content col-md-6 col-xl-7 py-md-4 px-4 pb-2">
+          <div class="d-flex justify-content-start mb-1">
+            <h5 class="flex-shrink-1 m-0"> PlayStation 4 1TB (Jet Black)</h5>
           </div>
-          &nbsp;
-          <a href="#" class="text-muted small">23</a>
+          <div style="margin-top: 30px;">
+            <div style="margin-bottom: 5px;"><span class="text-muted">CPU:</span>&nbsp; x86-64 AMD “Jaguar”, 8 cores</div>
+            <div style="margin-bottom: 5px;"><span class="text-muted">GPU:</span>&nbsp; 1.84 TFLOPS, AMD Radeon™ based graphics engine</div>
+            <div style="margin-bottom: 5px;"><span class="text-muted">Memory:</span>&nbsp; GDDR5 8GB</div>
+            <div style="margin-bottom: 5px;"><span class="text-muted">Storage size:</span>&nbsp; 1TB</div>
+          </div>
         </div>
-        <div class="small"><span class="text-muted">CPU:</span>&nbsp; x86-64 AMD “Jaguar”, 8 cores</div>
-        <div class="small"><span class="text-muted">GPU:</span>&nbsp; 1.84 TFLOPS, AMD Radeon™ based graphics engine</div>
-        <div class="small"><span class="text-muted">Memory:</span>&nbsp; GDDR5 8GB</div>
-        <div class="small"><span class="text-muted">Storage size:</span>&nbsp; 1TB</div>
+        <div class="d-flex col-md-3 col-xl-3 flex-md-column flex-wrap justify-content-md-center align-items-center px-3 px-md-0 pt-md-4 pb-4">
+          <div class="text-big m-2"><strong>300 RWT</strong></div>
+          <button class="button-normal">구매하기</button>
+        </div>
       </div>
-      <div class="d-flex col-md-3 col-xl-3 flex-md-column flex-wrap justify-content-md-center align-items-center px-3 px-md-0 pt-md-4 pb-4">
-        <div class="text-big m-2"><strong>$480.00</strong></div>
-        <a href="#" class="btn btn-outline-primary btn-sm text-expanded my-1 mx-2">TO CART</a>
-        <div class="text-muted small m-2"> 7 items available</div>
+      <div class="shop-list-row row row-bordered no-gutters">
+        <a href="#" class="d-block col-md-3 col-xl-2 text-center p-4 pr-md-0">
+          <img src="@/assets/img2/uikit/ps4.jpg" alt class="img-fluid" style="max-height: 190px">
+        </a>
+        <div class="shop-list-content col-md-6 col-xl-7 py-md-4 px-4 pb-2">
+          <div class="d-flex justify-content-start mb-1">
+            <h5 class="flex-shrink-1 m-0"> PlayStation 4 1TB (Jet Black)</h5>
+          </div>
+          <div style="margin-top: 30px;">
+            <div style="margin-bottom: 5px;"><span class="text-muted">CPU:</span>&nbsp; x86-64 AMD “Jaguar”, 8 cores</div>
+            <div style="margin-bottom: 5px;"><span class="text-muted">GPU:</span>&nbsp; 1.84 TFLOPS, AMD Radeon™ based graphics engine</div>
+            <div style="margin-bottom: 5px;"><span class="text-muted">Memory:</span>&nbsp; GDDR5 8GB</div>
+            <div style="margin-bottom: 5px;"><span class="text-muted">Storage size:</span>&nbsp; 1TB</div>
+          </div>
+        </div>
+        <div class="d-flex col-md-3 col-xl-3 flex-md-column flex-wrap justify-content-md-center align-items-center px-3 px-md-0 pt-md-4 pb-4">
+          <div class="text-big m-2"><strong>300 RWT</strong></div>
+          <button class="button-normal">구매하기</button>
+        </div>
+      </div>
+      <div class="shop-list-row row row-bordered no-gutters">
+        <a href="#" class="d-block col-md-3 col-xl-2 text-center p-4 pr-md-0">
+          <img src="@/assets/img2/uikit/ps4.jpg" alt class="img-fluid" style="max-height: 190px">
+        </a>
+        <div class="shop-list-content col-md-6 col-xl-7 py-md-4 px-4 pb-2">
+          <div class="d-flex justify-content-start mb-1">
+            <h5 class="flex-shrink-1 m-0"> PlayStation 4 1TB (Jet Black)</h5>
+          </div>
+          <div style="margin-top: 30px;">
+            <div style="margin-bottom: 5px;"><span class="text-muted">CPU:</span>&nbsp; x86-64 AMD “Jaguar”, 8 cores</div>
+            <div style="margin-bottom: 5px;"><span class="text-muted">GPU:</span>&nbsp; 1.84 TFLOPS, AMD Radeon™ based graphics engine</div>
+            <div style="margin-bottom: 5px;"><span class="text-muted">Memory:</span>&nbsp; GDDR5 8GB</div>
+            <div style="margin-bottom: 5px;"><span class="text-muted">Storage size:</span>&nbsp; 1TB</div>
+          </div>
+        </div>
+        <div class="d-flex col-md-3 col-xl-3 flex-md-column flex-wrap justify-content-md-center align-items-center px-3 px-md-0 pt-md-4 pb-4">
+          <div class="text-big m-2"><strong>300 RWT</strong></div>
+          <button class="button-normal">구매하기</button>
+        </div>
+      </div>
+      <div class="shop-list-row row row-bordered no-gutters">
+        <a href="#" class="d-block col-md-3 col-xl-2 text-center p-4 pr-md-0">
+          <img src="@/assets/img2/uikit/ps4.jpg" alt class="img-fluid" style="max-height: 190px">
+        </a>
+        <div class="shop-list-content col-md-6 col-xl-7 py-md-4 px-4 pb-2">
+          <div class="d-flex justify-content-start mb-1">
+            <h5 class="flex-shrink-1 m-0"> PlayStation 4 1TB (Jet Black)</h5>
+          </div>
+          <div style="margin-top: 30px;">
+            <div style="margin-bottom: 5px;"><span class="text-muted">CPU:</span>&nbsp; x86-64 AMD “Jaguar”, 8 cores</div>
+            <div style="margin-bottom: 5px;"><span class="text-muted">GPU:</span>&nbsp; 1.84 TFLOPS, AMD Radeon™ based graphics engine</div>
+            <div style="margin-bottom: 5px;"><span class="text-muted">Memory:</span>&nbsp; GDDR5 8GB</div>
+            <div style="margin-bottom: 5px;"><span class="text-muted">Storage size:</span>&nbsp; 1TB</div>
+          </div>
+        </div>
+        <div class="d-flex col-md-3 col-xl-3 flex-md-column flex-wrap justify-content-md-center align-items-center px-3 px-md-0 pt-md-4 pb-4">
+          <div class="text-big m-2"><strong>300 RWT</strong></div>
+          <button class="button-normal">구매하기</button>
+        </div>
       </div>
     </div>
-    <hr class="m-0">
-
-    <div class="shop-list-row row row-bordered no-gutters">
-      <a href="#" class="d-block col-md-3 col-xl-2 text-center p-4 pr-md-0">
-        <img src="assets/img/uikit/backpack.jpg" alt class="img-fluid" style="max-height: 190px">
-      </a>
-      <div class="shop-list-content col-md-6 col-xl-7 py-md-4 px-4 pb-2">
-        <div class="d-flex justify-content-start mb-1">
-          <h5 class="flex-shrink-1 m-0">
-            <a href="#" class="text-dark">HERO ATHLETES BAG</a>
-          </h5>
-          <a href="#" class="shop-tooltip d-inline-block text-lightest small ml-4" title="Share"><i class="ion ion-md-share"></i></a>
-          <a href="#" class="shop-tooltip d-inline-block text-lightest small ml-3" title="Add to wishlist"><i class="ion ion-ios-heart"></i></a>
-        </div>
-        <div class="mb-2">
-          <div class="ui-stars">
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star half-filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-          </div>
-          &nbsp;
-          <a href="#" class="text-muted small">23</a>
-        </div>
-        <div class="small"><span class="text-muted">Weight:</span>&nbsp; 2kg</div>
-        <div class="small"><span class="text-muted">Dimension:</span>&nbsp; L490 x W280 x H700mm</div>
-        <div class="small"><span class="text-muted">Fabrics:</span>&nbsp; POLYESTER 600D RIPSTOP</div>
-      </div>
-      <div class="d-flex col-md-3 col-xl-3 flex-md-column flex-wrap justify-content-md-center align-items-center px-3 px-md-0 pt-md-4 pb-4">
-        <div class="text-big m-2"><strong>$160.00</strong></div>
-        <a href="#" class="btn btn-outline-primary btn-sm text-expanded my-1 mx-2">TO CART</a>
-        <div class="text-muted small m-2"> 7 items available</div>
-      </div>
-    </div>
-    <hr class="m-0">
-
-    <div class="shop-list-row row row-bordered no-gutters">
-      <a href="#" class="d-block col-md-3 col-xl-2 text-center p-4 pr-md-0">
-        <img src="assets/img/uikit/iwatch.jpg" alt class="img-fluid" style="max-height: 190px">
-      </a>
-      <div class="shop-list-content col-md-6 col-xl-7 py-md-4 px-4 pb-2">
-        <div class="d-flex justify-content-start mb-1">
-          <h5 class="flex-shrink-1 m-0">
-            <a href="#" class="text-dark">Apple iWatch (black)</a>
-          </h5>
-          <a href="#" class="shop-tooltip d-inline-block text-lightest small ml-4" title="Share"><i class="ion ion-md-share"></i></a>
-          <a href="#" class="shop-tooltip d-inline-block text-lightest small ml-3" title="Add to wishlist"><i class="ion ion-ios-heart"></i></a>
-        </div>
-        <div class="mb-2">
-          <div class="ui-stars">
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star half-filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-          </div>
-          &nbsp;
-          <a href="#" class="text-muted small">23</a>
-        </div>
-        <div class="small"><span class="text-muted">Storage Capacity:</span>&nbsp; 8GB</div>
-        <div class="small"><span class="text-muted">Screen Size:</span>&nbsp; 1.65"</div>
-        <div class="small"><span class="text-muted">Connections:</span>&nbsp; USB, Wi-Fi, Bluetooth 4.0</div>
-        <div class="small"><span class="text-muted">Sensors:</span>&nbsp; Accelerometer, Ambient light sensor, Gyroscope, Heart rate sensor</div>
-      </div>
-      <div class="d-flex col-md-3 col-xl-3 flex-md-column flex-wrap justify-content-md-center align-items-center px-3 px-md-0 pt-md-4 pb-4">
-        <div class="text-big m-2"><strong>$399.00</strong></div>
-        <a href="#" class="btn btn-outline-primary btn-sm text-expanded my-1 mx-2">TO CART</a>
-        <div class="text-muted small m-2"> 7 items available</div>
-      </div>
-    </div>
-    <hr class="m-0">
-
-    <div class="shop-list-row row row-bordered no-gutters">
-      <a href="#" class="d-block col-md-3 col-xl-2 text-center p-4 pr-md-0">
-        <img src="assets/img/uikit/s7edge-1.jpg" alt class="img-fluid" style="max-height: 190px">
-      </a>
-      <div class="shop-list-content col-md-6 col-xl-7 py-md-4 px-4 pb-2">
-        <div class="d-flex justify-content-start mb-1">
-          <h5 class="flex-shrink-1 m-0">
-            <a href="#" class="text-dark">Samsung Galaxy S7 Edge</a>
-          </h5>
-          <a href="#" class="shop-tooltip d-inline-block text-lightest small ml-4" title="Share"><i class="ion ion-md-share"></i></a>
-          <a href="#" class="shop-tooltip d-inline-block text-lightest small ml-3" title="Add to wishlist"><i class="ion ion-ios-heart"></i></a>
-        </div>
-        <div class="mb-2">
-          <div class="ui-stars">
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-            <div class="ui-star half-filled">
-              <i class="ion ion-md-star"></i>
-              <i class="ion ion-md-star"></i>
-            </div>
-          </div>
-          &nbsp;
-          <a href="#" class="text-muted small">23</a>
-        </div>
-        <div class="small"><span class="text-muted">Processor Speed, Type:</span>&nbsp; 2.15GHz, 1.6GHz, Quad-Core</div>
-        <div class="small"><span class="text-muted">Internal Memory:</span>&nbsp; 32GB</div>
-        <div class="small"><span class="text-muted">Main Display Size:</span>&nbsp; 5.5” (139.5mm)</div>
-        <div class="small"><span class="text-muted">Camera Resolution (Rear):</span>&nbsp; Dual Pixel 12.0 MP</div>
-      </div>
-      <div class="d-flex col-md-3 col-xl-3 flex-md-column flex-wrap justify-content-md-center align-items-center px-3 px-md-0 pt-md-4 pb-4">
-        <div class="text-big m-2"><strong>$1049.00</strong></div>
-        <a href="#" class="btn btn-outline-primary btn-sm text-expanded my-1 mx-2">TO CART</a>
-        <div class="text-muted small m-2"> 7 items available</div>
-      </div>
-    
-    <hr class="m-0">
-
-      
-    </div>
-
   </div>
-        <!-- / Products -->
 </template>
 
 <script>
@@ -225,6 +99,5 @@ export default {
 <style scoped>
   .blank{
     padding: 50px;
-
   }
 </style>
